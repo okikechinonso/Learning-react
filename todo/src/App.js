de101ebd7c78f9c9580002adccbfccdece1f111d
Okike Chinonso <Chinonsookike@gmail.com>
2021-11-12 22:59:57 +0100

    worked on a todo using react

---
+++ todo/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import AddTodo from './AddTodo.js';
+import TaskList from './taskList.js';
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false },
+];
+
+function App(){
+  const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(title){
+    setTodos([
+      ...todos,{
+        id: nextId++,
+        title,
+        done: false
+      }
+    ]);
+  }
+
+  function handleChangeTodo(nextTodo){
+    setTodos(todos.map(t => {
+      if (t.id === nextTodo.id){
+        return nextTodo;
+      }else{
+        return t;
+      }
+    }));
+  }
+
+  function handleDelete(todoId){
+    setTodos(todos.filter(t => t.id !== todoId))
+  }
+
+  return (
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+
+      <TaskList todos={todos} 
+      onChageTodo={handleChangeTodo}
+      onDeleteTodo = {handleDelete}
+    />
+    </>
+  )
+}
+export default App;
